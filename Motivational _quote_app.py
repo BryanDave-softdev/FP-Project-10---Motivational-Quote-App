5f3af9d562b3d5a354e953dd08250330bd507d9e
Bryan Dave <hbryandave411@gmail.com>
2025-07-29 07:20:28 +0800

    Motivational _quote_app.py

---
+++ Motivational _quote_app.py
@@ -0,0 +1,21 @@
+import random
+
+# 🎯 List of motivational quotes
+quotes = [
+    "Success is not final, failure is not fatal, failure is not fatal: it is the courage to continue that counts.",
+    "Believe in yourself and all that you are. know that there is something inside you greater than any obstacle.",
+    "Don't watch the clock; do what it does, keep going.",
+    "The harder you work for something, the greater you'll feel when you achieve it.",
+    "Dream bigger, Do bigger.",
+    "Push yourself, because no one else is going to do it for you.",
+    "sSometimes we're tested not to show our weaknesses, but to dixcover our strengths.",
+    "Wake up with determination. Go to bed with satisfaction.",
+    "It's going to be hard, but hard does not mean impossible."
+]
+
+print("==============================")
+print(" Welcome to the Motivation Hub")
+print("==============================")
+
+random_quote = random.choice(quotes)
+print(random_quote)
